24f2a351be7484f90aae9fa96126202d0dabd334
AKINCI <ozgurakinci@outlook.com>
2022-10-25 19:01:36 +0300

    changed gradle file.

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM registry.access.redhat.com/ubi8/openjdk-17
+
+ENV JAVA_APP_DIR="/deployments" \
+    JAVA_APP_JAR="${APP_NAME}.jar" \
+    JAVA_MAJOR_VERSION="11" \
+    TZ="Eurepa/Istanbul"
+
+RUN curl -o /deployments/${APP_NAME}.jar ${ARTIFACT_URL} --insecure
+
+RUN chown /deployments/${APP_NAME}.jar
+COPY application.sh /deployments/application.sh
+
+CMD ["sh", "/deployments/application.sh"]
---
+++ application.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd /deployments
+
+JAVA_OPTIONS="$OTHER_JAVA_OPTIONS $JAVA_OPTIONS"
+JAVA_OPTIONS="$JAVA_OPTIONS -Dspring.profiles.default=$ACTIVE_PROFILE -Dspring.profiles.active=$ACTIVE_PROFILE"
+if [ ! -z $SERVER_PORT ]
+then
+        JAVA_OPTIONS="$JAVA_OPTIONS -Dserver.port=$SERVER_PORT"
+else
+        JAVA_OPTIONS="$JAVA_OPTIONS -Dserver.port=8080"
+fi
+
+
+echo "Starting the Java application using /opt/run-java/run-java.sh ..."
+exec /opt/run-java/run-java.sh
